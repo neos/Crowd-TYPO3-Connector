4bb6bf10bc6895265f97d31029d17e64a8d2aeda
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2014-06-21 22:40:40 +0200

    [TASK] add compiled files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TYPO3 Jira Authentication Plugin
+# TYPO3 SSO Atlassian Crowd Authentication Plugin
 
 In order to compile, run the following command:
 
@@ -6,4 +6,14 @@ In order to compile, run the following command:
 ./gradlew clean jar
 `
 
-Then, you find the jar file inside `build/libs/`.
+Then, you need to copy the following two files into Crowd:
+
+`
+cp build/libs/typo3-auth.jar /[path-to-crowd]/crowd-webapp/WEB-INF/lib/
+cp json-simple-1.1.1.jar /[path-to-crowd]/crowd-webapp/WEB-INF/lib/
+`
+
+After that, restart crowd.
+
+* Then, the Directory Class is: org.typo3.sso.crowd.TYPO3SsoDirectory
+* Set the API key in an attribute called "apiKey"
--- src/main/java/org/typo3/sso/crowd/TYPO3SsoDirectory.java
+++ src/main/java/org/typo3/sso/crowd/TYPO3SsoDirectory.java
@@ -20,29 +20,40 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Connector to TYPO3 SSO
  */
 public class TYPO3SsoDirectory extends InternalDirectory {
 
-    private static final Logger logger = LoggerFactory.getLogger(InternalDirectory.class);// configuration parameters
+	private static final Logger logger = LoggerFactory.getLogger(InternalDirectory.class);// configuration parameters
+	private HashMap<String, String> attributes = new HashMap<String, String>();
 
-    public TYPO3SsoDirectory(InternalDirectoryUtils internalDirectoryUtils, PasswordEncoderFactory passwordEncoderFactory, DirectoryDao directoryDao, UserDao userDao, GroupDao groupDao, MembershipDao membershipDao) {
-        super(internalDirectoryUtils, passwordEncoderFactory, directoryDao, userDao, groupDao, membershipDao);
-    }
 
-		/*
-		 * Since we extend Internal Directory, and groups will be managed in this directory,
-		 * not on typo3.org, we can use Crowd's nested groups support.
-		 */
-		public boolean supportsNestedGroups() {
-				return true;
+	public TYPO3SsoDirectory(InternalDirectoryUtils internalDirectoryUtils, PasswordEncoderFactory passwordEncoderFactory, DirectoryDao directoryDao, UserDao userDao, GroupDao groupDao, MembershipDao membershipDao) {
+		super(internalDirectoryUtils, passwordEncoderFactory, directoryDao, userDao, groupDao, membershipDao);
+	}
+
+	@Override
+	public void setAttributes(Map<String, String> attrs) {
+		for (Map.Entry<String, String> entry : attrs.entrySet()) {
+			attributes.put(entry.getKey(), entry.getValue());
 		}
+	}
+
+	/*
+			 * Since we extend Internal Directory, and groups will be managed in this directory,
+			 * not on typo3.org, we can use Crowd's nested groups support.
+			 */
+	public boolean supportsNestedGroups() {
+		return true;
+	}
 
 		/*
 		 * attributes
-		 *   ATTRIBUTE_PASSWORD_COMPLEXITY_MESSAGE 
+		 *   ATTRIBUTE_PASSWORD_COMPLEXITY_MESSAGE
 		 *   ATTRIBUTE_PASSWORD_HISTORY_COUNT
 		 *   ATTRIBUTE_PASSWORD_MAX_ATTEMPTS
 		 *   ATTRIBUTE_PASSWORD_MAX_CHANGE_TIME
@@ -53,89 +64,93 @@ public class TYPO3SsoDirectory extends InternalDirectory {
 		 * What about default groups like the Internal Directory?
 		 */
 
-    @Override
-    public User authenticate(String username, PasswordCredential credentials)
-            throws InactiveAccountException, InvalidAuthenticationException,
-            ExpiredCredentialException, UserNotFoundException {
+	@Override
+	public User authenticate(String username, PasswordCredential credentials)
+			throws InactiveAccountException, InvalidAuthenticationException,
+			ExpiredCredentialException, UserNotFoundException {
 
-        try {
-            HttpClient client = new HttpClient();
+		try {
+			HttpClient client = new HttpClient();
 
-            BufferedReader br = null;
-
-            PostMethod method = new PostMethod("https://typo3.org/services/authenticate.php");
-            method.addParameter("apiKey", "9ac8173cabf179a1277190a71b6dd009187cabbe7e1ff78a43278919489aac28da10cff83cad8194622b3bc1983710a2");
-            method.addParameter("username", username);
-            method.addParameter("returnUserInfo", "1");
-            method.addParameter("password", credentials.getCredential());
-
-            String responseString = "";
-
-            try {
-                int returnCode = client.executeMethod(method);
-                logger.info("Authentication request returned with code: " + returnCode);
-
-                br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
-                String readLine;
-                while(((readLine = br.readLine()) != null)) {
-                    logger.info(readLine);
-                    responseString = responseString.concat(readLine);
-                }
-            } catch (Exception e) {
-                System.err.println(e);
-            } finally {
-                method.releaseConnection();
-                if(br != null) try { br.close(); } catch (Exception fe) {}
-            }
-
-            if (responseString.equals("0")) {
-                // Authentication not successful
-                throw new InvalidAuthenticationException("Auth Failed");
-            } else {
-                User user;
-                try {
-                    user = findUserByName(username);
-                    return user;
-                } catch (UserNotFoundException e) {
-                    //add the user to Crowd if it's not there, and populate with default groups.
-                    //(groups will be managed in Crowd, then pushed to Jira)
-
-                    JSONObject parsedResponseString = (JSONObject) JSONValue.parse(responseString);
-                    logger.info("PARSED RESPONSE: " + parsedResponseString);
-
-                    if (!username.equals(parsedResponseString.get("username"))) {
-                        throw new InvalidAuthenticationException("Something went terribly wrong");
-                    }
-
-                    UserTemplate userTemplate = new UserTemplate(username);
-                    userTemplate.setFirstName((String) parsedResponseString.get("firstName"));
-                    userTemplate.setLastName((String) parsedResponseString.get("lastName"));
-                    userTemplate.setDisplayName((String) parsedResponseString.get("name"));
-                    userTemplate.setEmailAddress((String) parsedResponseString.get("email"));
-                    userTemplate.setDirectoryId(getDirectoryId());
-
-                    user = addUser(userTemplate, null); // null password in Crowd.
-                    return user;
+			BufferedReader br = null;
 
 
-                    //We might need to add default groups eventually, but Crowd might be able to do that automatically, so we'll ignore it for now.
-                    //get defaultGroups
-                    //for (String groupname : defaultGroups) {
-                    //	addUserToGroup(username, groupName);
-                    //}
-                    //throw new UserNotFoundException("User not found");
-                }
-            }
-        } catch (InvalidUserException e1) {
-            e1.printStackTrace();
-        } catch (UserAlreadyExistsException e1) {
-            e1.printStackTrace();
-        } catch (OperationFailedException e1) {
-            e1.printStackTrace();
-        } catch (InvalidCredentialException e1) {
-            e1.printStackTrace();
-        }
+			PostMethod method = new PostMethod("https://typo3.org/services/authenticate.php");
+			method.addParameter("apiKey", attributes.get("apiKey"));
+			method.addParameter("username", username);
+			method.addParameter("returnUserInfo", "1");
+			method.addParameter("password", credentials.getCredential());
 
-        return null;
-    }
+			String responseString = "";
+
+			try {
+				int returnCode = client.executeMethod(method);
+				logger.info("Authentication request returned with code: " + returnCode);
+
+				br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
+				String readLine;
+				while (((readLine = br.readLine()) != null)) {
+					logger.info(readLine);
+					responseString = responseString.concat(readLine);
+				}
+			} catch (Exception e) {
+				System.err.println(e);
+			} finally {
+				method.releaseConnection();
+				if (br != null) try {
+					br.close();
+				} catch (Exception fe) {
+				}
+			}
+
+			if (responseString.equals("0")) {
+				// Authentication not successful
+				throw new InvalidAuthenticationException("Auth Failed");
+			} else {
+				User user;
+				try {
+					user = findUserByName(username);
+					return user;
+				} catch (UserNotFoundException e) {
+					//add the user to Crowd if it's not there, and populate with default groups.
+					//(groups will be managed in Crowd, then pushed to Jira)
+
+					JSONObject parsedResponseString = (JSONObject) JSONValue.parse(responseString);
+					logger.info("PARSED RESPONSE: " + parsedResponseString);
+
+					if (!username.equals(parsedResponseString.get("username"))) {
+						throw new InvalidAuthenticationException("Something went terribly wrong");
+					}
+
+					UserTemplate userTemplate = new UserTemplate(username);
+					userTemplate.setFirstName((String) parsedResponseString.get("firstName"));
+					userTemplate.setLastName((String) parsedResponseString.get("lastName"));
+					userTemplate.setDisplayName((String) parsedResponseString.get("name"));
+					userTemplate.setEmailAddress((String) parsedResponseString.get("email"));
+					userTemplate.setDirectoryId(getDirectoryId());
+
+					user = addUser(userTemplate, null); // null password in Crowd.
+					return user;
+
+
+					//We might need to add default groups eventually, but Crowd might be able to do that automatically, so we'll ignore it for now.
+					//get defaultGroups
+					//for (String groupname : defaultGroups) {
+					//	addUserToGroup(username, groupName);
+					//}
+					//throw new UserNotFoundException("User not found");
+				}
+			}
+		} catch (InvalidUserException e1) {
+			e1.printStackTrace();
+		} catch (UserAlreadyExistsException e1) {
+			e1.printStackTrace();
+		} catch (OperationFailedException e1) {
+			e1.printStackTrace();
+		} catch (InvalidCredentialException e1) {
+			e1.printStackTrace();
+		}
+
+		return null;
+	}
 }
